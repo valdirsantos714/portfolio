64d5df48baf2bdc63a8f6700e9a9a46ffe0e2cc5
Valdir Santos <valdirsantost40@gmail.com>
2024-07-17 12:45:21 -0300

    Adicionado mais um curso da alura

--- src/views/TelaInicial/components/Educacao/Educacao.jsx
+++ src/views/TelaInicial/components/Educacao/Educacao.jsx
@@ -2,7 +2,7 @@ import "./Educacao.css"
 
 export default function Educacao() {
 
-    const cursosAlura = ["React: construindo componentes com JSX,JavaScript: conhecendo arrays,React: construíndo componentes com JSX,React: desenvolvendo em React Router com JavaScript,PostgreSQL: desenvolva com PL/pgSQL Java: consumindo API gravando arquivos e lidando com erros, JavaScript: programando a Orientação a Objetos, JavaScript: validações e reconhecimento de voz, Lógica de programação: mergulhe em programação com JavaScript, Html e CSS: Responsividade com Mobile-First, CSS: Flexbox e Layouts Responsivos,HTML E CSS: Trabalhando com Responsividade e Publicação de Projetos, PostgreSQL"];
+    const cursosAlura = ["Spring Boot 3: aplique boas práticas e proteja uma API Rest, React: construindo componentes com JSX,JavaScript: conhecendo arrays,React: construíndo componentes com JSX,React: desenvolvendo em React Router com JavaScript,PostgreSQL: desenvolva com PL/pgSQL Java: consumindo API gravando arquivos e lidando com erros, JavaScript: programando a Orientação a Objetos, JavaScript: validações e reconhecimento de voz, Lógica de programação: mergulhe em programação com JavaScript, Html e CSS: Responsividade com Mobile-First, CSS: Flexbox e Layouts Responsivos,HTML E CSS: Trabalhando com Responsividade e Publicação de Projetos, PostgreSQL"];
 
     const cursosUdemy = ["Git Completo: Do Básico ao Avançado, Java COMPLETO Programação Orientada a Objetos + Projetos"]
 
