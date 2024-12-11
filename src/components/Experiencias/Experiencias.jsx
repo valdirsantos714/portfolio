34d2a667164c7fcd8ad48bb6e946e196294d6f27
Valdir Santos <valdirsantost40@gmail.com>
2024-12-11 11:25:57 -0300

    feat: adicionada mais experiência

--- src/components/Experiencias/Experiencias.jsx
+++ src/components/Experiencias/Experiencias.jsx
@@ -9,13 +9,17 @@ export default function Experiencias() {
 
                     <li className="bordas1">
 
-                        <h3 className="h3-negrito">Desenvolvedor Full Stack Voluntário na Codigo Certo Coders <br /> <span className="italico">Remoto, Meio Período</span>
+                        <h3 className="h3-negrito">Desenvolvedor Java na Squadra Digital <br /> <span className="italico">Remoto, Meio Período</span>
                         </h3>
 
-                        <span className="italico">Agosto 2024 – Setembro 2024</span>
+                        <span className="italico">Outubro 2024 – Presente</span>
 
-                        <p>Desenvolvo e mantenho aplicações back-end, garantindo a eficiência do servidor, usando Java, Spring Boot e Spring Security. <br /> Crio e executo testes automatizados para assegurar a qualidade do software, utilizando JUnit e Mockito. <br /> Participo de revisões de código, garantindo boas práticas de desenvolvimento.  <br /> Colaboro com o time de Front-End, auxiliando na implementação de interfaces, integrações, responsividade, usabilidade e performance, utilizando React JS, Javascript, Typescript e Tailwind CSS.</p>
+                        <p>Bootcamp intensivo em Java do Programa New Thinkers 2024 da empresa Squadra Digital, no qual estou aprimorando minhas habilidades técnicas <br />e desenvolvendo projetos reais utilizando Java, em um ambiente colaborativo e dinâmico, ao lado de outros profissionais e sob orientação direta de um instrutor.<br /> <br /> 
+                        Esse programa de treinamento inclui estudos aprofundados em lógica de programação, Banco de Dados Oracle, JDBC, desenvolvimento de APIs RESTful, e experiência prática com Spring Framework e Spring Boot, com foco em aplicações robustas e escaláveis.
+                        <br /> <br /> 
+                        Além das competências técnicas, o bootcamp enfatiza o desenvolvimento de habilidades interpessoais e soft skills, como trabalho em equipe, resiliência, adaptabilidade e comunicação eficaz. Estou evoluindo em áreas como criatividade, pensamento crítico, ética profissional, liderança humanizada e resolução de problemas, habilidades fundamentais para um ambiente corporativo moderno. <br /> <br /> Esse treinamento também me oferece prática em gestão de tempo, autogestão e foco em resultados, fortalecendo minha capacidade de adaptação em projetos de alta demanda.</p>
                     </li>
+
 
                     <li className="bordas1">
                         
@@ -25,8 +29,18 @@ export default function Experiencias() {
                         <span className="italico"> Itapororoca PB <br /> Maio 2023 - Presente</span>
                         <p>Implemento soluções completas e integradas, desde o frontend até o backend. <br /> Utilizo tecnologias como React JS, TypeScript, Tailwind CSS, HTML, CSS no desenvolvimento de interfaces modernas e responsivas. No backend, uso Java, Spring Boot, Spring Security, e arquitetura de microservices para criar APIs robustas e escaláveis. <br /> Implemento e gerencio bancos de dados como PostgreSQL e MySQL, e utilizo ferramentas de versionamento como Git.
                         </p>
-                    </li>                    
+                    </li>
 
+                    <li className="bordas1">
+
+                        <h3 className="h3-negrito">Desenvolvedor Full Stack Voluntário na Codigo Certo Coders <br /> <span className="italico">Remoto, Meio Período</span>
+                        </h3>
+
+                        <span className="italico">Agosto 2024 – Setembro 2024</span>
+
+                        <p>Desenvolvo e mantenho aplicações back-end, garantindo a eficiência do servidor, usando Java, Spring Boot e Spring Security. <br /> Crio e executo testes automatizados para assegurar a qualidade do software, utilizando JUnit e Mockito. <br /> Participo de revisões de código, garantindo boas práticas de desenvolvimento.  <br /> Colaboro com o time de Front-End, auxiliando na implementação de interfaces, integrações, responsividade, usabilidade e performance, utilizando React JS, Javascript, Typescript e Tailwind CSS.</p>
+                    </li>
+                                    
                     <li className="bordas1">
                         <h3 className="h3-negrito">Caixa em Severino Motos <br /> <span className="italico">Presencial, Tempo Integral</span>
                         </h3>
